a009c5bf569d427087464db123151071e339e72d
Fomagran <fomagran6@naver.com>
2021-05-29 12:54:37 +0900

    음악 소리에 따라서 건물 나오게 하기

--- WaterReflectionSound/ViewController.swift
+++ WaterReflectionSound/ViewController.swift
@@ -6,16 +6,92 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    // Configuration Settings
+    private let updateInterval = 1
+    private let animatioтDuration = 0.05
+    private let maxPowerDelta: CGFloat = 30
+    private let minScale: CGFloat = 0.9
 
-    let heights = [100,120,80,60,140,100,120,80,60,140,100,120,80,60,140,100,120,80,60,140,100,120,80,60,140,100,120,80,60,140,100,120,80,60,140,100,120,80,60,140,100,120,80,60,140]
+    // Internal Timer to schedule updates from player
+    private var timer: Timer?
+
+    // Ingected Player to get power Metrics
+    var player: AVAudioPlayer!
+
+    var heights:[CGFloat]!
+    var time:Int = 0
     
     @IBOutlet weak var buildingCollection: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        playSound()
+        start()
+       
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "track", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            guard let player = player else { return }
+            let duration = player.duration
+            heights = Array(repeating: 0, count:Int(floor(duration + 1)) )
+            player.isMeteringEnabled = true
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func start() {
+        timer = Timer.scheduledTimer(timeInterval: TimeInterval(updateInterval),
+                                     target: self,
+                                     selector: #selector(self.updateMeters),
+                                     userInfo: nil,
+                                     repeats: true)
+    }
+
+    func stop() {
+        guard timer != nil, timer!.isValid else {
+            return
+        }
+
+        timer?.invalidate()
+        timer = nil
+    }
+
+    // Animate self transform depends on player meters
+    @objc private func updateMeters() {
+        player.updateMeters()
+        let power = averagePowerFromAllChannels()
+
+        UIView.animate(withDuration: animatioтDuration, animations: {
+            self.heights[self.time] = floor(abs(power))*10
+            self.buildingCollection.reloadItems(at: [IndexPath(item: self.time, section: 0)])
+            self.time += 1
+        }) { (_) in
+            if !self.player.isPlaying {
+                self.stop()
+            }
+        }
+    }
+
+    // Calculate average power from all channels
+    private func averagePowerFromAllChannels() -> CGFloat {
+        var power: CGFloat = 0.0
+        (0..<player.numberOfChannels).forEach { (index) in
+            power = power + CGFloat(player.averagePower(forChannel: index))
+        }
+        return power
     }
 }
 
